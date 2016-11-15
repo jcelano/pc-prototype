bd6d668d2c82ac4899d85a8b86ab096881b235b9
Joe Celano <joecelano@gmail.com>
2016-11-15 17:20:06 -0500

    made web app work stand alone

--- server/app/assets/js/components/services/WebFileService.js
+++ server/app/assets/js/components/services/WebFileService.js
@@ -3,7 +3,7 @@ var pcs = angular.module('procClinSafeApp');
 
 pcs.service('FileService', ['$http', function($http) {
 
-    const CLIENTS_FILE = "https://desolate-sierra-98327.herokuapp.com/clients.json";// userDataDir.path() + path.sep + DATA_DIR + path.sep + "clients.json";
+    const CLIENTS_FILE = " https://dry-waters-26669.herokuapp.com/clients.json";// userDataDir.path() + path.sep + DATA_DIR + path.sep + "clients.json";
 
     const REPORTS_DIR = "https://desolate-sierra-98327.herokuapp.com/reports";
 
